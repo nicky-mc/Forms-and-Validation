0922010f83da351a396accbabbc4873f6e8a0984
nicky-mc <cinimod@gmail.com>
2024-10-09 11:57:34 +0100

    server setup

---
+++ forms/Relational-Database/Server/SERVER.md
@@ -0,0 +1,10 @@
+# Set up
+
+1. npm init -y
+2. npm i express cors pg dotenv
+3. Added "type": "module: and "dev": "node.server"
+4. add .gitignore and .env
+5. inside .gitignore put in node_modules and .env
+6. Now we write in our server.js our dependencies --> look in comments in server.js
+7. Test your server end point
+8. You can use a seed.js to run your initial SQL queries, especially the ones to CREATE our initial database tables or use the SQL editor in Supabase
---
+++ forms/Relational-Database/Server/server.js
@@ -0,0 +1,38 @@
+// we need to install express, cors, dotenv, pg
+// we import our dependencies
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pg from "pg";
+
+// need to configure our .env file
+dotenv.config();
+
+// we create a new express app
+const app = express();
+
+// tell express to use json
+app.use(express.json());
+// tell express to use cors
+app.use(cors());
+
+// creating a new pool for my database connection
+const dbConnectionString = process.env.DB_URL;
+if (!dbConnectionString) {
+  throw new Error("DB_URL is not defined in the environment variables");
+}
+
+// creating a new pool for my database connection
+export const db = new pg.Pool({ connectionString: dbConnectionString });
+
+// we define our port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT} and built on Rock and Roll`);
+});
+// now write an end point for our root route to test our server
+app.get("/", (req, res) => {
+  res.json({ message: "HAIL to the Queen baby" });
+});
+// we can use thunder client to ensure our server is working
+// we can now run our server using node server.js
